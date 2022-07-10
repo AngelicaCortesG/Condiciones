4a4c35f36e1d1e2f92a646ae15dff38190014f12
Angelica <a.cortes.godoy@gmail.com>
2022-07-10 11:19:02 -0400

    edit css

--- password/assets/js/script3.js
+++ password/assets/js/script3.js
@@ -3,12 +3,17 @@ function calcular() {
     select2 = document.querySelector('#select-2')
     select3 = document.querySelector('#select-3')
     span = document.querySelector('span')
-    
-     if (select1.value == 9 && select2.value == 1 && select3.value == 1) {
-        span = document.querySelector('span').textContent = 'Password 1 correcto'
+    if (select1.value == 9 && select2.value == 1 && select3.value == 1) {
+        span = document.querySelector('span')
+        span.innerHTML = 'Password 1 correcto'
+        span.style.color= "green"
     } else if (select1.value == 7 && select2.value == 1 && select3.value == 4) {
-        span = document.querySelector('span').textContent = 'Password 2 correcto'
+        span = document.querySelector('span')
+        span.innerHTML = 'Password 2 correcto'
+        span.style.color= "green"
     } else {
-        span = document.querySelector('span').textContent = 'Password incorrecto';
+        span = document.querySelector('span')
+        span.innerHTML = 'Password 1 correcto'
+        span.style.color= "red"
     }
 }
