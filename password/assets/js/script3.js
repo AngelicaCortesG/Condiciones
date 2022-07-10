20dcfb356eacf6bd40f3da517824a15680150ff9
Angelica <a.cortes.godoy@gmail.com>
2022-07-09 23:41:23 -0400

    add condition js

---
+++ password/assets/js/script3.js
@@ -0,0 +1,14 @@
+function calcular() {
+    select1 = document.querySelector('#select-1')
+    select2 = document.querySelector('#select-2')
+    select3 = document.querySelector('#select-3')
+    span = document.querySelector('span')
+    
+     if (select1.value == 9 && select2.value == 1 && select3.value == 1) {
+        span = document.querySelector('span').textContent = 'Password 1 correcto'
+    } else if (select1.value == 7 && select2.value == 1 && select3.value == 4) {
+        span = document.querySelector('span').textContent = 'Password 2 correcto'
+    } else {
+        span = document.querySelector('span').textContent = 'Password incorrecto';
+    }
+}
